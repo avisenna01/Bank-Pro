05447d12225f282b69097bfea3bd351e1074e237
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-11-19 18:16:48 +0700

    initial homepage

---
+++ src/views/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+const HomePage = props => {
+
+
+    return (
+        <Grid container>
+
+        </Grid>
+    )
+
+
+}
+
+export default HomePage;
