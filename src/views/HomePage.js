7a3330d329f4f2f3a2f2ebf464c81241c5ab5a0f
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-11-19 20:19:29 +0700

    update

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
-const HomePage = props => {
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        border: "5px black"
+    },
+}));
 
+
+const HomePage = props => {
+    const classes = useStyles();
 
     return (
-        <Grid container>
-
-        </Grid>
+        <div className={classes.root}>
+            <Grid container xs={12}>
+                <Grid item>lesgo</Grid>
+            </Grid>
+        </div>
     )
 
 
