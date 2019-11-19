7e3f0fcd69f897d2fa9d80c847281f0806f5ff02
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-11-19 20:41:34 +0700

    styling homepage

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -12,7 +13,7 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-        border: "5px solid black"
+        border: "5px solid #66BBFF"
     },
 }));
 
@@ -22,10 +23,22 @@ const HomePage = props => {
 
     return (
         <div className={classes.root}>
-            <Grid container xs={12} spacing={3}>
-                <Grid item xs={12}><Paper className={classes.paper}>lesgo</Paper></Grid>
-                <Grid item xs={12} md={6}><Paper className={classes.paper}>lesgo</Paper></Grid>
-                <Grid item xs={12} md={6}><Paper className={classes.paper}>lesgo</Paper></Grid>
+            <Grid container xs={12} >
+                <Grid style={{ marginBottom: "55px" }} item xs={12}><Paper className={classes.paper}>
+                    <Typography variant="h5" gutterBottom>Nama pemilik rekening</Typography>
+                    <Typography variant="subtitle1" gutterBottom>Nomor Rekening: 987656789876</Typography>
+                    <Typography variant="subtitle1" gutterBottom>Nama Bank: BNI</Typography>
+                    <Typography variant="subtitle1" gutterBottom>Saldo rekening: Rp. 1000.000</Typography>
+
+                </Paper></Grid>
+                <Grid style={{ marginLeft: "1px" }} container xs={12} spacing={3}>
+                    <Grid item xs={12} md={6}><Paper className={classes.paper}>
+                        <Typography variant="h5" gutterBottom>Riwayat Transaksi</Typography>
+                    </Paper></Grid>
+                    <Grid item xs={12} md={6}><Paper className={classes.paper}>
+                        <Typography variant="h5" gutterBottom>Transfer</Typography>
+                    </Paper></Grid>
+                </Grid>
             </Grid>
         </div>
     )
