44837cd0f3a38d24b64ff736d08e4ab4fdf4f458
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-11-20 10:43:42 +0700

    update tables

---
+++ src/views/History.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        width: '100%',
+        marginTop: theme.spacing(3),
+        overflowX: 'auto',
+        paddingTop: "150px",
+        paddingBottom: "150px"
+
+    },
+    table: {
+        minWidth: 700,
+    },
+
+}));
+
+const StyledTableCell = withStyles(theme => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default,
+        },
+    },
+}))(TableRow);
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+const History = props => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Grid container justify="center" alignItems="center">
+                <Grid item>
+                    <Table className={classes.table} aria-label="simple table">
+                        <TableHead>
+                            <StyledTableRow>
+                                <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                                <StyledTableCell align="right">Calories</StyledTableCell>
+                                <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+                                <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+                                <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+                            </StyledTableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map(row => (
+                                <StyledTableRow key={row.name}>
+                                    <StyledTableCell component="th" scope="row">
+                                        {row.name}
+                                    </StyledTableCell>
+                                    <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                                    <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                                </StyledTableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Grid>
+            </Grid>
+        </div>
+    )
+
+
+}
+
+export default History;
