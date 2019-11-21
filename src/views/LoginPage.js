5a1710429c4d162790fbc8384fc94ecd8c05dbc3
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-11-21 19:01:04 +0700

    updates 19.01

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -39,9 +39,74 @@ const LoginPage = props => {
 
   const classes = useStyles();
 
+  // const submitLogin = () => {
+  //   const url = "ini url";
+
+  //   console.log(this.state.loginEmail);
+  //   console.log(this.state.loginPass);
+
+  //   fetch("ini url")
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       const that = this;
+  //       const len = res.data.length;
+  //       const checklist = [];
+  //       for (let i = 0; i < len; i++) {
+  //         checklist.push(res.data[i].user_email);
+  //       }
+  //       console.log(res)
+  //       console.log(checklist);
+  //       if (checklist.includes(this.state.loginEmail)) {
+  //         const params = {
+  //           user_email: this.state.loginEmail,
+  //           password: this.state.loginPass
+  //         };
+
+  //         fetch(url, {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json"
+  //           },
+  //           body: JSON.stringify(params)
+  //         })
+  //           .then(function (response) {
+  //             return response.json();
+  //           })
+  //           .then(function (json) {
+  //             console.log(json);
+  //             if (json.data.token == null) {
+  //               alert("Wrong password");
+  //             } else {
+  //               cookies.set('token', json.data.token, { path: '/' });
+  //               that.setState({
+  //                 token: json.data.token,
+  //                 redirect: true
+  //               });
+  //               console.log(cookies.get('token'));
+  //               alert("Login success!");
+  //               console.log(json.data.token);
+  //               console.log(json.data)
+  //               console.log(that.state.token)
+  //               console.log("sena keren")
+  //               alert("ale caur")
+  //             }
+  //           });
+  //       } else {
+  //         alert("Email not detected!");
+  //       }
+  //     });
+  // };
+
   const handleChange = event => {
     console.log(event.target.value)
   };
+
+  const _handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      // this.submitLogin()
+      console.log("ini sena lesgo")
+    }
+  }
 
 
   return (
@@ -50,7 +115,7 @@ const LoginPage = props => {
         <Typography className={classes.title}>Bank Pro</Typography>
       </Grid>
       <Grid item xs={12} container justify="center" alignItems="center" style={{ paddingBottom: "400px" }}>
-        <input onChange={handleChange} style={inputBox} type="text" placeholder="Account Number" />
+        <input onKeyDown={_handleKeyDown} onChange={handleChange} style={inputBox} type="text" placeholder="Account Number" />
       </Grid>
     </Grid>
   );
