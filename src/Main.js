f1050c519997399ac5199ba3ec5e5af4dbafdf4b
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-11-24 15:43:23 +0700

    updates

--- src/Main.js
+++ src/Main.js
@@ -14,10 +14,10 @@ const Main = () => {
       <Switch>
         <Grid>
           <Header />
-          <Route exact path="/" component={HomePage} />
+          <Route exact path="/" component={AuthRoute(HomePage)} />
           <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/home" component={HomePage} />
-          <Route exact path="/history" component={History} />
+          <Route exact path="/home" component={AuthRoute(HomePage)} />
+          <Route exact path="/history" component={AuthRoute(History)} />
           {/* <Route path="/404" component={NotFound} />
           <Redirect to="/404" /> */}
         </Grid>
