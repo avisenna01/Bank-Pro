cc5a40fd8ef18cc8b4f8e58dfb10e16b9d29d637
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-11-24 15:43:23 +0700

    updates

--- src/Main.js
+++ src/Main.js
@@ -5,21 +5,19 @@ import Grid from "@material-ui/core/Grid";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./views/HomePage";
 import History from "./views/History";
-// import AuthRoute from "./AuthRoute"
+import AuthRoute from "./AuthRoute"
 // import NotFound from "./views/NotFound";
 
 const Main = () => {
-
-
   return (
     <Router>
       <Switch>
         <Grid>
           <Header />
-          <Route exact path="/" component={HomePage} />
+          <Route exact path="/" component={AuthRoute(HomePage)} />
           <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/home" component={HomePage} />
-          <Route exact path="/history" component={History} />
+          <Route exact path="/home" component={AuthRoute(HomePage)} />
+          <Route exact path="/history" component={AuthRoute(History)} />
           {/* <Route path="/404" component={NotFound} />
           <Redirect to="/404" /> */}
         </Grid>
